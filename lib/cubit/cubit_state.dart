51c93624b6bb864a8d600dfcd2bac5f164c07365
SardorbekFlutterDeveloper <sardorbek9884@gmail.com>
2022-04-07 11:20:33 +0500

    second commit

--- lib/cubit/cubit_state.dart
+++ lib/cubit/cubit_state.dart
@@ -2,6 +2,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TypingCubit extends Cubit<TypingState> {
   TypingCubit() : super(TypingInitial());
+  int? count;
+  
+  List<String> randomSoz = [
+    "Salom", "Hello", "Ipsum", "Lorem", "Snake4"
+  ];
+  void fix(List<String> words) {
+    num jami = 0;
+    
+  }
 }
 
 abstract class TypingState {
@@ -11,5 +20,3 @@ abstract class TypingState {
 class TypingInitial extends TypingState {
   TypingInitial();
 }
-
-
--- lib/cubit/cubit_view.dart
+++ lib/cubit/cubit_view.dart
@@ -1,91 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-class TypingGame extends StatelessWidget {
-  const TypingGame({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  TextEditingController controller = TextEditingController();
+  AnimationController? animationController;
+  String tx =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do laborum. ";
+  List gap = [];
+  bool col = true;
+
+  List l = [];
+
+  @override
+  void initState() {
+    super.initState();
+    gap = tx.split('');
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 15),
+      lowerBound: 0,
+      upperBound: 400,
+    );
+    animationController!.addListener(() {
+      setState(() {});
+    });
+    animationController!.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => MemorizeCubit(),
-      child: BlocConsumer<MemorizeCubit, MemorizeState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return myScaffold(context);
-        },
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: col ? Colors.yellow : Colors.red,
+        title: Text(
+          "Typing Game",
+          style: TextStyle(color: Colors.greenAccent, fontSize: 30),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: Colors.white,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      // right: animationController!.value,
+                      child: Center(
+                        child: Text(
+                          tx,
+                          style: const TextStyle(
+                            fontSize: 30,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                controller: controller,
+                cursorColor: col ? Colors.yellow : Colors.red,
+                cursorHeight: 30,
+                onChanged: (v) {
+                  l = v.split('');
+                  for (var i = 0; i < gap.length; i++) {
+                    if (gap[i] == l[i]) {
+                      setState(() {
+                        col = true;
+                      });
+
+                      animationController!.stop();
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //     const SnackBar(content: Text('Winner')));
+                    } else {
+                      setState(() {
+                        col = false;
+                      });
+                    }
+                  }
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  animationController!.repeat();
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15),),),),
+                child: const Icon(Icons.restart_alt_rounded),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  Scaffold myScaffold(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.greenAccent,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Memorize Game",
-          style: TextStyle(fontSize: 30, color: Colors.green),
-        ),
-      ),
-      backgroundColor: Colors.greenAccent,
-      body: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.all(10.0),
-            child: InkWell(
-              child: CircleAvatar(
-                radius: 35,
-                backgroundColor: Colors.green,
-                
-                child: context.watch<MemorizeCubit>().isVisible
-                    ? Text(
-                        context
-                            .watch<MemorizeCubit>()
-                            .randoms[index]
-                            .toString(),
-                        style: TextStyle(color: Colors.white, fontSize: 25),
-                      )
-                    : const Text(""),
-              ),
-              onTap: () {
-                context.read<MemorizeCubit>().ochir(index);
-              },
-            ),
-          );
-        },
-        itemCount: context.watch<MemorizeCubit>().randoms.length,
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 40),
-            child: FloatingActionButton(
-              backgroundColor: Colors.green,
-              elevation: 2,
-              focusColor: Colors.lightBlue,
-              onPressed: () {
-                context.read<MemorizeCubit>().makeRandom();
-               
-              },
-              child: Icon(Icons.replay_circle_filled_rounded ),
-            ),
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.green,
-            elevation: 2,
-            splashColor: Colors.white,
-            onPressed: () {
-              context.read<MemorizeCubit>().showRandoms();
-            },
-            child: Icon(Icons.play_arrow),
-          ),
-        ],
-      ),
-    );
+  @override
+  void dispose() {
+    super.dispose();
+    animationController!.dispose();
   }
 }
